689b81a04acad7a6a603533279cc04f20c7657c4
Glitch (hello-express) <none>
2020-04-08 07:28:00 +0000

    👸🗺 Checkpoint ./package.json:2476407/4403 ./server.js:2476407/2143

--- server.js
+++ server.js
@@ -3,10 +3,50 @@ const client = new Discord.Client();
 client.login(process.env.TOKEN);
 
 const express = require("express");
+const fetch = require('node-fetch');
+const btoa = require('btoa'),
+      session = require("express-session"),
+      MemcachedStore = require("connect-memcached")(session);
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: "CatOnKeyboard",
+    key: "test",
+    proxy: "true",
+    resave: false,
+    saveUninitialized: false,
+    store: new MemcachedStore({
+      hosts: ["127.0.0.1:11211"],
+      secret: "123, easy as ABC. ABC, easy as 123" // Optionally use transparent encryption for memcache session data
+    })
+  })
+);
+app.use((err, req, res, next) => {
+  switch (err.message) {
+    case 'NoCodeProvided':
+      return res.status(400).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+    default:
+      return res.status(500).send({
+        status: 'ERROR',
+        error: err.message,
+      });
+  }
+});
+
+const catchAsync = fn => (
+  (req, res, next) => {
+    const routePromise = fn(req, res, next);
+    if (routePromise.catch) {
+      routePromise.catch(err => next(err));
+    }
+  }
+);
 
 const redirect = 'https://flamecord.glitch.me/api/discord/callback';
 
@@ -16,9 +56,21 @@ app.get("/", (req, res) => {
 app.get('/api/discord/login', (req, res) => {
   res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${encodeURIComponent(redirect)}`);
 });
-app.get('/api/discord/callback', (req, res) => {
-  
-})
+app.get('/api/discord/callback', catchAsync(async (req, res) => {
+  if (!req.query.code) throw new Error('NoCodeProvided');
+  const code = req.query.code;
+  const creds = btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${creds}`,
+      },
+    });
+  const json = await response.json();
+  console.log(json);
+  res.redirect(`/?token=${json.access_token}`);
+}));
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
