fb704c08ca2f31d024cfafa42c7668ebbe15b0bc
Glitch (hello-express) <none>
2020-04-08 07:16:44 +0000

    💰🍈 Checkpoint ./views/discordCallback.ejs:2476407/1432 ./server.js:2476407/806 ./views/login.ejs:2476407/217 ./views/layouts/head.ejs:2476407/7

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const redirect = 'https://flamecord.glitch.me/api/discord/callback';
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+app.get('/api/discord/login', (req, res) => {
+  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${encodeURIComponent(redirect)}`);
+});
+app.get('/api/discord/callback', (req, res) => {
+  
+})
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
