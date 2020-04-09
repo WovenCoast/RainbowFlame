ac654df031f959da365c951035dd05b0db5a8c4c
Glitch (hello-express) <none>
2020-04-09 15:33:56 +0000

    🌛🍞 Checkpoint ./commands/Music/play.js:2476407/2177 ./package.json:2476407/1643 ./watch.json:2476407/2

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,4 +1,6 @@
+
 const ytdl = require('ytdl-core');
+const search = require('yt-search');
 
 module.exports = {
   name: "play",
@@ -6,8 +8,17 @@ module.exports = {
   desc: "Play a song of your choice from YouTube",
   cooldown: 3,
   async exec(client, message, args) {
-    if (!client.queue.has(message.guild.id)) {
+    if (!args[0]) throw new Error("You need to provide a search string or a YouTube URL!");
+    let url = args;
+    if (!args[0].startsWith("http")) {
+      let res = await search(args.join(" "));
+      let videos = res.videos.slice(0, 10);
+      const requestMsg = message.channel.send(new videos.map((video, index) => `**${index + 1}**: ${video.title.includes(video.author.name) ? video.title.replace(video.author.name, `**${video.author.name}**`) : `**${video.title}** by *${video.author.name}*`}`));
       
     }
+    let serverQueue = client.queue.get(message.guild.id)
+    if (!serverQueue) {
+    }
+    
   }
 }
