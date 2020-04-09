067a440402a336c2a92691b8779308f0d92c84eb
Glitch (hello-express) <none>
2020-04-09 15:21:52 +0000

    🖐💵 Checkpoint ./commands/Music/play.js:2476407/1459 ./package.json:2476407/1589 ./server.js:2476407/125

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,9 +1,13 @@
+const ytdl = require('ytdl-core');
+
 module.exports = {
   name: "play",
   aliases: ["p"],
   desc: "Play a song of your choice from YouTube",
   cooldown: 3,
   async exec(client, message, args) {
-    
+    if (!client.queue.has(message.guild.id)) {
+      
+    }
   }
 }
